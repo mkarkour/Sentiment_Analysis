d564fe55f2a5b6fe2690c4fee5b1a7c195acebc2
Mehdi-Ulb <83882339+Mehdi-Ulb@users.noreply.github.com>
2024-01-28 22:08:19 +0100

    testing yaml

--- src/my_app.py
+++ src/my_app.py
@@ -1,4 +1,18 @@
+import streamlit as st
+import pandas as pd
+import yaml
 
 
 def printing() -> str:
     return 'Hello World'
+
+def display(df: pd.DataFrame) -> None:
+    st.dataframe(df)
+    #st.table(data.iloc[0:10])
+    st.json({'foo':'bar','fu':'ba'})
+    st.metric(label="Temp", value="273 K", delta="1.2 K")
+
+if __name__ == '__main__':
+    with open("credentials.yaml",'r') as f:
+        credentials = yaml.load(f)
+        print(credentials)
