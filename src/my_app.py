df3f2f58ca791d1d7910cbcbd1639371ae66404b
Mehdi-Ulb <83882339+Mehdi-Ulb@users.noreply.github.com>
2024-01-30 10:58:21 +0100

    adding changing my_app

--- src/my_app.py
+++ src/my_app.py
@@ -6,13 +6,22 @@ import yaml
 def printing() -> str:
     return 'Hello World'
 
+
 def display(df: pd.DataFrame) -> None:
     st.dataframe(df)
     #st.table(data.iloc[0:10])
     st.json({'foo':'bar','fu':'ba'})
     st.metric(label="Temp", value="273 K", delta="1.2 K")
 
+
+def get_credentials(site: str) -> dict:
+    with open(r"C:\Users\karme\Sentiment_Analysis\credentials.yaml",'r') as f:
+        credentials = yaml.load(f,Loader=yaml.FullLoader)
+        cred = credentials[site]
+        return cred 
+
+
 if __name__ == '__main__':
-    with open("credentials.yaml",'r') as f:
-        credentials = yaml.load(f)
-        print(credentials)
+    df = pd.read_csv(r"C:\Users\karme\Sentiment_Analysis\data\test\BBCA.JK new.csv")
+    display(df)
+    
